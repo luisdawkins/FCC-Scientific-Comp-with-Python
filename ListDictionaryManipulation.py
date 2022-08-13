5890dce278e27c1a48656ad06f0f9f617ee0e286
tHanTnIE1234 <110572931+tHanTnIE1234@users.noreply.github.com>
2022-08-13 10:10:34 -0400

    Create ListDictionaryManipulation.py

---
+++ ListDictionaryManipulation.py
@@ -0,0 +1,17 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  77: 1450,
+  "Maximus": ["Batman", "Superman", "Green Lantern", "Aquaman"]
+  
+}
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+print(thisdict["brand"])
+print(thisdict[77])
+print(thisdict["Maximus"])
+
+thislist.append("Overwatch")
+print(thislist)
